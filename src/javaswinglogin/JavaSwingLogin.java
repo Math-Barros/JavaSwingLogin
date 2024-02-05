2676e8765786722c01596e0d865219d732200a3b
Matheus Barros <math__barros@outlook.com>
2024-02-05 15:21:27 -0300

    first commit

---
+++ src/javaswinglogin/JavaSwingLogin.java
@@ -0,0 +1,15 @@
+
+package javaswinglogin;
+
+public class JavaSwingLogin {
+
+
+    public static void main(String[] args) {
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
